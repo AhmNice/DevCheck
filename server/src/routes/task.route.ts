80b6b4a591e67d77be82e5b99da44983ba3bd209
AhmNice <talk2muhammedawwal@gmail.com>
2026-03-04 01:05:31 +0100

    Update task schema and routes   - Added status, priority, and user_id fields to task schema   - Updated task routes to include session verification   - Sanitized task input to prevent XSS attacks

--- server/src/routes/task.route.ts
+++ server/src/routes/task.route.ts
@@ -10,16 +10,57 @@ import {
 } from "../controllers/task.controller.js";
 import { jsonTaskHandler } from "../utils/json_task_handler.js";
 import { singleUpload } from "../middleware/file_uploader.js";
+import {
+  validateId,
+  validateSubtaskInput,
+  validateTaskInput,
+  validationResultHandler,
+} from "../utils/inputValidator.js";
+import { verifySession } from "../middleware/verifysession.js";
 
 const taskRouter = express.Router();
 
-taskRouter.post("/create", createTask);
-taskRouter.post("/:task_id/subtasks", saveSubtask);
+taskRouter.post(
+  "/create",
+  validateTaskInput,
+  validationResultHandler,
+  createTask,
+);
+taskRouter.post(
+  "/:task_id/subtasks",
+  validateSubtaskInput,
+  validationResultHandler,
+  saveSubtask,
+);
 taskRouter.post("/import", singleUpload, jsonTaskHandler);
-taskRouter.get("/get/:task_id", getTaskWithSubtasks);
-taskRouter.get("/user/:user_id", getTasksByUserId);
-taskRouter.delete("/delete/:task_id", deleteTask);
-taskRouter.delete("/delete/subtask/:subtask_id", deleteSubtask);
-taskRouter.put("/update/:task_id", updateTask);
+taskRouter.get(
+  "/get/:task_id",
+  validateId("task_id"),
+  validationResultHandler,
+  verifySession,
+  getTaskWithSubtasks,
+);
+taskRouter.get(
+  "/user/:user_id",
+  validateId("user_id"),
+  validationResultHandler,
+  verifySession,
+  getTasksByUserId,
+);
+taskRouter.delete(
+  "/delete/:task_id",
+  validateId("task_id"),
+  validationResultHandler,
+  verifySession,
+  deleteTask,
+);
+taskRouter.delete("/delete/subtask/:subtask_id", verifySession, deleteSubtask);
+taskRouter.put(
+  "/update/:task_id",
+  validateId("task_id"),
+  validationResultHandler,
+  verifySession,
+  updateTask,
+);
 
 export default taskRouter;
